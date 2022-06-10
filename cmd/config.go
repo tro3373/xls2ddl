47b406c269f11e78c5e4c6e705b37c210475de27
tro3373 <tro3373@gmail.com>
2022-06-10 20:48:58 +0900

    feat: Generate init

---
+++ cmd/config.go
@@ -0,0 +1,14 @@
+package cmd
+
+type Config struct {
+	IgnoreSheet         []string
+	TableCell           string
+	TableNameCell       string
+	ColumnStartCell     string
+	ColumnNameStartCell string
+	TypeStartCell       string
+	DigitStartCell      string
+	DecimalStartCell    string
+	NNStartCell         string
+	PKStartCell         string
+}
